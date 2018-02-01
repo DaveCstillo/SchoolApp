28f06899baa70cd6a69eb7b695ba19b8547df5f9
David Castillo <davecstillo@gmail.com>
2018-01-31 20:39:02 -0600

    Initial commit

---
+++ app/src/main/java/app/davecstillo/com/schoolapp/MainActivity.java
@@ -0,0 +1,17 @@
+package app.davecstillo.com.schoolapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Fra
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
